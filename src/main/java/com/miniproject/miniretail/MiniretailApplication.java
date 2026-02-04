3dbcf018e838e8fe39d6ed2f489a631a77e6e429
amaliadhanira <46930528+amaliadhanira@users.noreply.github.com>
2026-02-04 23:15:02 +0700

    initial commit

---
+++ src/main/java/com/miniproject/miniretail/MiniretailApplication.java
@@ -0,0 +1,13 @@
+package com.miniproject.miniretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniretailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniretailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miniproject/miniretail/MiniretailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproject.miniretail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniretailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
